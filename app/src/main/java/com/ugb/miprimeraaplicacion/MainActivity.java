c4da218b72b2833821c65e249339f40a76a1e9c2
Luis Hernandez <sarco_luis@hotmail.com>
2025-02-03 07:25:40 -0600

    Uso del spinner en la calculadora

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -17,8 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
-    RadioGroup rgb;
-    RadioButton opt;
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,21 +35,20 @@ public class MainActivity extends AppCompatActivity {
 
                 double respuesta = 0.0;
 
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    respuesta = num1 + num2;
-                }
-                opt = findViewById(R.id.optResta);
-                if (opt.isChecked()) {
-                    respuesta = num1 - num2;
-                }
-                opt = findViewById(R.id.optMultiplicacion);
-                if (opt.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-                opt = findViewById(R.id.optDivision);
-                if (opt.isChecked()) {
-                    respuesta = num1 / num2;
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
+                        respuesta = num1 + num2;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
                 }
                 tempVal = findViewById(R.id.lblRespuesta);
                 tempVal.setText("Respuesta: "+ respuesta);
